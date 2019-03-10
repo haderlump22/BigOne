fdf96c1f045a1d07b50c3607bf9c33c4a2399ede
Normen Rachel <haderlump22@gmx.de>
2019-03-10 16:01:05 +0100

    correct some Mistakes

--- src/de/rachel/bigone/Renderer/ValuesTableCellRenderer.java
+++ src/de/rachel/bigone/Renderer/ValuesTableCellRenderer.java
@@ -13,42 +13,48 @@ public class ValuesTableCellRenderer implements TableCellRenderer {
    public Component getTableCellRendererComponent(
       JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
 
-      JLabel label;
+      JLabel label = null;
       
-      if(!(value instanceof JLabel)) {
-         label = new JLabel(value.toString());
-      } else {
-         label = (JLabel) value;
-      }
-      label.setOpaque(true);
-      label.setFont(table.getFont());
-      label.setForeground(table.getForeground());
-      label.setBackground(table.getBackground());
+      // only react when value is not null
+      if (value != null) {
+          if(!(value instanceof JLabel)) {
+              label = new JLabel(value.toString());
+           } else {
+              label = (JLabel) value;
+           }
+           label.setOpaque(true);
+           label.setFont(table.getFont());
+           label.setForeground(table.getForeground());
+           label.setBackground(table.getBackground());
 
-      if(isSelected) {
-         label.setBackground(table.getSelectionBackground());
-         label.setForeground(table.getSelectionForeground());
+           if(isSelected) {
+              label.setBackground(table.getSelectionBackground());
+              label.setForeground(table.getSelectionForeground());
+           }
+           if(column == 0) {
+              label.setHorizontalAlignment(JLabel.RIGHT);
+           }
+           if(column == 1) {
+     	     label.setHorizontalAlignment(JLabel.CENTER);
+     	  }
+           if(column == 2) {
+     	     label.setHorizontalAlignment(JLabel.CENTER);
+     	     label.setText(BigOneTools.datum_wandeln(value.toString(),1));
+     	  }
+           if(column == 3) {
+              label.setHorizontalAlignment(JLabel.RIGHT);
+              label.setText(value.toString().replace('.', ','));
+           }
+           if(column == 5) {
+     	     label.setHorizontalAlignment(JLabel.CENTER);
+     	     if(value != null) {//nur wenn es ein liquidatum gibt umwandeln
+     	    	 label.setText(BigOneTools.datum_wandeln(value.toString(),1));
+     	     }
+     	  }
+           return label;
+      } else {
+    	  return label;
       }
-      if(column == 0) {
-         label.setHorizontalAlignment(JLabel.RIGHT);
-      }
-      if(column == 1) {
-	     label.setHorizontalAlignment(JLabel.CENTER);
-	  }
-      if(column == 2) {
-	     label.setHorizontalAlignment(JLabel.CENTER);
-	     label.setText(BigOneTools.datum_wandeln(value.toString(),1));
-	  }
-      if(column == 3) {
-         label.setHorizontalAlignment(JLabel.RIGHT);
-         label.setText(value.toString().replace('.', ','));
-      }
-      if(column == 5) {
-	     label.setHorizontalAlignment(JLabel.CENTER);
-	     if(value != null) {//nur wenn es ein liquidatum gibt umwandeln
-	    	 label.setText(BigOneTools.datum_wandeln(value.toString(),1));
-	     }
-	  }
-      return label;
+
    }
 }
