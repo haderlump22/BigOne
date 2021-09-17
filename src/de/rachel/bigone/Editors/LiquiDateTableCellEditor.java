35968bafa8ded2d7705953321a707e6627389fc6
Normen Rachel <normen.rachel@gmx.de>
2021-09-17 17:40:53 +0200

    Eingabeformat für das Liquidatum
    - in der Importtabelle wird das jetzt analog dem Datum in "Beträge finden"
    gehandhabt

--- src/de/rachel/bigone/Editors/LiquiDateTableCellEditor.java
+++ src/de/rachel/bigone/Editors/LiquiDateTableCellEditor.java
@@ -1,10 +1,12 @@
 package de.rachel.bigone.Editors;
 
 import java.awt.Component;
+import java.text.ParseException;
 
 import javax.swing.AbstractCellEditor;
 import javax.swing.JTable;
-import javax.swing.JTextField;
+import javax.swing.JFormattedTextField;
+import javax.swing.text.MaskFormatter;
 import javax.swing.table.TableCellEditor;
 
 import de.rachel.bigone.BigOneTools;
@@ -14,21 +16,25 @@ public class LiquiDateTableCellEditor extends AbstractCellEditor implements Tabl
 	 * 
 	 */
 	private static final long serialVersionUID = 4917922491523056278L;
-	private JTextField component = new JTextField(); 
-	 
-	  public Component getTableCellEditorComponent( 
-	      JTable table, Object value, boolean isSelected, int rowIndex, int colIndex ) { 
-	    	component.setText("");	//sicherheitshalber leer machen da sonst bei erneutem aufruf
-	    							//und leerem value der letzte wert zurueckgegeben wird
-		  	if(value != null)
-	    		component.setText(BigOneTools.datum_wandeln(value.toString(),1));
-	    	return component; 
-	  } 
-	  public Object getCellEditorValue() 
-	  { 
-		  if(component.getText().length() > 0)
-			  return BigOneTools.datum_wandeln(component.getText(),0); 
-		  else
-			  return null;
-	  }
+	private JFormattedTextField component = new JFormattedTextField();
+
+	public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int rowIndex, int colIndex) {
+		// eingabeformat vorbelegen
+		try {
+			component = new JFormattedTextField(new MaskFormatter("01.##.20##"));
+		} catch (ParseException e1) {
+			e1.printStackTrace();
+		}
+		component.setText("");	//sicherheitshalber leer machen da sonst bei erneutem aufruf
+								//und leerem value der letzte wert zurueckgegeben wird
+		if(value != null)
+			component.setText(BigOneTools.datum_wandeln(value.toString(),1));
+		return component;
+	} 
+	public Object getCellEditorValue() {
+		if (component.getText().length() > 0)
+			return BigOneTools.datum_wandeln(component.getText(), 0);
+		else
+			return null;
+	}
 }
