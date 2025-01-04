2e33e804f1470373ecdf56e1bc2eb57cb76a6715
Normen Rachel <normen.rachel@gmx.de>
2025-01-04 18:56:58 +0100

    DBTools angepasst um im TableModel mit den Daten des Resultsets arbeiten zu können

--- src/de/rachel/bigone/DBTools.java
+++ src/de/rachel/bigone/DBTools.java
@@ -6,70 +6,76 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public final class DBTools {
-	private Statement  st = null;
-	private ResultSet  rs = null;
+	private Statement st = null;
+	private ResultSet rs = null;
 	private Connection cn = null;
+	private int RowCount;
 	private Object[][] daten;
 
-	public DBTools(Connection LoginCN) { 
+	public DBTools(Connection LoginCN) {
 		try {
 			cn = LoginCN;
-			st = cn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
-		}
-		catch(Exception ex) {
+			st = cn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		} catch (Exception ex) {
 			System.out.println(ex.toString());
-		} 
+		}
 	}
+
 	public boolean insert(String sql) {
 		try {
 			st.executeUpdate(sql);
 			return true;
-		}
-		catch(SQLException ex) {
+		} catch (SQLException ex) {
 			ex.printStackTrace();
 			return false;
 		}
 	}
+
 	public boolean update(String sql) {
 		try {
 			st.executeUpdate(sql);
 			return true;
-		}
-		catch(SQLException ex) {
+		} catch (SQLException ex) {
 			ex.printStackTrace();
 			return false;
 		}
 	}
+
 	public void select(String sql, int iFields) {
 		try {
-			rs = st.executeQuery( sql );
+			rs = st.executeQuery(sql);
 
-			//auf Grund der uebergebenen Feldanzahl und der ermittelten
-			//datensaetze das array redimensionieren
+			// auf Grund der uebergebenen Feldanzahl und der ermittelten
+			// datensaetze das array redimensionieren
 			rs.last();
-			daten = new Object[rs.getRow()][iFields];
+			RowCount = rs.getRow();
+			daten = new Object[RowCount][iFields];
 			rs.beforeFirst();
-			
-			//daten aus dem recordset in das stringarray lesens
-			while ( rs.next() ) {
-				for(int i=0; i<iFields ; i++) {
-					daten[rs.getRow()-1][i] = rs.getObject(i+1);}
+
+			// daten aus dem recordset in das stringarray lesens
+			while (rs.next()) {
+				for (int i = 0; i < iFields; i++) {
+					daten[rs.getRow() - 1][i] = rs.getObject(i + 1);
+				}
 			}
-			
-			//recordset/statement wieder leeren
-			rs = null;
-		}
-		catch(SQLException ex) {
+		} catch (SQLException ex) {
 			ex.printStackTrace();
-		} 
+		}
 	}
-	public Object[][] getData(){
+
+	public Object[][] getData() {
 		return daten;
 	}
+
 	public int getRowCount() {
-		return daten.length;
+		return RowCount;
 	}
+
 	public Object getValueAt(int row, int col) {
 		return daten[row][col];
+	}
+
+	public ResultSet getResultSet() {
+		return rs;
 	}
 }
