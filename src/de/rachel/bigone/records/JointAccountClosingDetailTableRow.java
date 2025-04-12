3e1182c6c18b6438144cbeaf6393a179115ebd2f
Normen Rachel <normen.rachel@gmx.de>
2025-04-12 11:16:01 +0200

    Record für die Tabelle des Haushaltsabschlusses erzeugt

---
+++ src/de/rachel/bigone/records/JointAccountClosingDetailTableRow.java
@@ -0,0 +1,5 @@
+package de.rachel.bigone.records;
+
+public record JointAccountClosingDetailTableRow(String NameOfExpenditure, Double ActualAmount, Double PlanAmount,
+        Double Difference) {
+};
