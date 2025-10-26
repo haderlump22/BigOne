8067c8d1c3cbb9e85aee524f087bab197a205aa5
Normen Rachel <normen.rachel@gmx.de>
2025-10-26 19:43:29 +0100

    SQL Sring mit Grossschreibung

--- src/de/rachel/bigone/Aufteilung.java
+++ src/de/rachel/bigone/Aufteilung.java
@@ -37,58 +37,58 @@ public class Aufteilung extends JFrame{
 	//damit ereignissid, betrag und liqui kennzeichen aufgenommen
 	//werden kann (in dieser reihenfolge)
 	private String[][] daten = null, lager = null;
-	
+
 	Aufteilung(JFrame dialogOwner, double flGesBetrag, String strBuchText, Connection LoginCN){
 		cn = LoginCN;
 		final JDialog dialog = new JDialog(dialogOwner, "Aufteilung", true);
 		dialog.setSize(300,250);
 		dialog.setLayout(null);
 		dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		
+
 		//das Array daten wird schon mal initialisiert
 		daten = new String[1][3];
-		
+
 		//die uebergebene Variable flGesBetrag in die in dieser
 		//Klasse ueberall leesbaren Variablen flGesBetLager kopieren
 		flGesBetLager = flGesBetrag;
-		
+
 		//schriftenfestlegungen
 		fontTxtFields = new Font("Arial",Font.PLAIN,16);
 		fontCmbBoxes = new Font("Arial",Font.PLAIN,16);
-		
+
 		//objekt festlegungen
 		chkLiquiFaehig = new JCheckBox("Liquifaehig",true);
 		chkLiquiFaehig.setBounds(80,10,110,25);
-		
+
 		lblBetrag = new JLabel("Betrag");
 		lblBetrag.setBounds(10,45,38,25);
-		
+
 		txtBetrag = new JFormattedTextField(new NumberFormatter(new DecimalFormat("#,##0.00")));
-		txtBetrag.setBounds(80,45,60,25);		
+		txtBetrag.setBounds(80,45,60,25);
 		txtBetrag.setHorizontalAlignment(JFormattedTextField.RIGHT);
 		txtBetrag.setFont(fontTxtFields);
 		txtBetrag.setText("0,00");
 		txtBetrag.addFocusListener( new FocusListener() {
 			public void focusLost( FocusEvent fe ) {
-		    	 
+
 			}
 			public void focusGained(FocusEvent fe) {
 				txtBetrag.selectAll();
 			}
 		});
-		
+
 		lblEreigniss = new JLabel("Ereigniss");
 		lblEreigniss.setBounds(10,80,53,25);
-		
+
 		cmbEreigniss = new JComboBox<String>();
 		cmbEreigniss.setFont(fontCmbBoxes);
 		cmbEreigniss.setBounds(80,80,200,25);
 		fill_cmbEreigniss();
-		
+
 		btnIncr = new JButton("neuer Datensatz");
 		btnIncr.setBounds(10,115,130,35);
-		btnIncr.addActionListener(new ActionListener(){ 
-            public void actionPerformed(ActionEvent ae){ 
+		btnIncr.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
             	//falls der aufteilungssatz niedriger oder gleich dem noch aufzuteilenden
             	//restbetrag ist werden die eingetragenen Werte des Formulars in den aktuell
             	//freien Datensatz des Arrays (dies ist immer der hoechste) gespeichert wenn
@@ -105,15 +105,15 @@ public class Aufteilung extends JFrame{
         			else
         				daten[daten.length - 1][2] = "FALSE";
         		}
-        		
+
         		//verringert den im Infofeld angegebenen Betrag um den eingegebenen
         		//und stellt den rest dar, dieser wird in der Variablen flGesBetLager
         		//gleich mitgespeichert fuer die naechste erhoehung
         		lblRestInfo.setText("noch " + String.valueOf(roundScale2(flGesBetLager - Double.valueOf(txtBetrag.getText().replace(".","").replace(',','.')).doubleValue())).replace('.',',') + " EUR");
         		flGesBetLager = roundScale2(flGesBetLager - Double.valueOf(txtBetrag.getText().replace(".","").replace(',','.')).doubleValue());
-        		
+
         		//sollte der Aufteilungsbetrag 0 sein (weil schon alles aufgeteilt wurde) wird
-        		//der plus button deaktiviert und der Focus auf den Speicherbutton gelegt 
+        		//der plus button deaktiviert und der Focus auf den Speicherbutton gelegt
         		if(flGesBetLager == 0) {
         			btnSave.setEnabled(true);
         			btnSave.requestFocus();
@@ -124,31 +124,31 @@ public class Aufteilung extends JFrame{
         		//Datensatz aufzunehmen
         		else {
         			erhoehe_datenfeld();
-        			
+
         		}
-        		
+
         		//nun werden noch die inhalte der Textfelder geleert
         		cmbEreigniss.setSelectedIndex(0);
         		txtBetrag.setText("0,00");
             }
         });
-		
+
 		btnSave = new JButton("Speichern");
 		btnSave.setBounds(150,115,130,35);
 		//damit nicht einfach so gespeichert werden kann
 		//wird der Button ersteinmal deaktiviert
 		//aktiviert wird er erst wieder wenn der gesammte aufteilungs-
-		//betrag bis auf 0 aufgeteilt ist (pruefung findet 
+		//betrag bis auf 0 aufgeteilt ist (pruefung findet
 		//beim klicken auf den Button "neuer Datensatz" statt)
 		btnSave.setEnabled(false);
 		btnSave.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
 				dialog.setVisible(false);
             	dialog.dispose();
-				
+
 			}
 		});
-		
+
 		lblRestInfo = new JLabel("noch " + String.valueOf(flGesBetLager).replace('.',',') + " EUR");
 		lblRestInfo.setBounds(10,160,290,25);
 		lblRestInfo.setHorizontalAlignment(JLabel.CENTER);
@@ -157,7 +157,7 @@ public class Aufteilung extends JFrame{
 		lblBuchText.setBounds(10,180,290,25);
 		lblBuchText.setHorizontalAlignment(JLabel.CENTER);
 		lblBuchText.setToolTipText(strBuchText);
-		
+
 		dialog.add(chkLiquiFaehig);
 		dialog.add(lblBetrag);
 		dialog.add(txtBetrag);
@@ -167,7 +167,7 @@ public class Aufteilung extends JFrame{
 		dialog.add(btnSave);
 		dialog.add(lblRestInfo);
 		dialog.add(lblBuchText);
-		
+
 		dialog.setVisible(true);
 	}
 	private String extractEreigId(String strEreigAusCmbBox) {
@@ -176,7 +176,7 @@ public class Aufteilung extends JFrame{
 	private void erhoehe_datenfeld() {
 		//erhoet das datenfeld daten um ein feld unter erhalt der bisher gespeicherten daten
 		int i;
-		
+
 		lager = new String[daten.length][3];
 		//elemente des datenfeldes in das lager zwischenkopieren
 		for (i = 0; i < daten.length; i++) {
@@ -201,10 +201,10 @@ public class Aufteilung extends JFrame{
 	private void fill_cmbEreigniss() {
 		DBTools getter = new DBTools(cn);
 
-		getter.select("SELECT ereigniss_id, ereigniss_krzbez FROM kontenereignisse WHERE gueltig = 'TRUE' order by 2;",2);
-		
+		getter.select("SELECT ereigniss_id, ereigniss_krzbez FROM kontenereignisse WHERE gueltig = 'TRUE' ORDER BY 2;",2);
+
 		Object[][] cmbValues = getter.getData();
-		
+
 		for(Object[] cmbValueParts : cmbValues) {
 			cmbEreigniss.addItem(cmbValueParts[1] + " (" + cmbValueParts[0]+")");
 		}
@@ -217,4 +217,3 @@ public class Aufteilung extends JFrame{
 		return daten;
 	}
 }
-
