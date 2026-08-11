ed2789bdfb5d4fe0818f0171627b37e7fddcef8f
Normen Rachel <normen.rachel@gmx.de>
2026-08-11 21:32:43 +0200

    Fertigstellung Maven Config
    - es wir jetzt bis zum app-image alles korrekt erzeugt und das programm kann über mvn exec:java gestartet werden

--- src/main/java/de/rachel/app/Login.java
+++ src/main/java/de/rachel/app/Login.java
@@ -3,6 +3,11 @@ package de.rachel.app;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -15,98 +20,123 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
+import com.google.gson.*;
+
 public class Login {
 
-    private JDialog login;
-    private int Logincount = 0;
-    private JLabel lblUeberschrift, lblName, lblPW;
-    private JTextField txtBenutzer;
-    private JPasswordField txtPW;
-    private JButton btnLogin;
-    private String strB, strPW;
-    private Connection cn = null;
+  private JDialog login;
+  private int Logincount = 0;
+  private JLabel lblUeberschrift, lblName, lblPW;
+  private JTextField txtBenutzer;
+  private JPasswordField txtPW;
+  private JButton btnLogin;
+  private String strB, strPW;
+  private Connection cn = null;
+  private Path configFile;
+  private boolean devMode = false;
+  private Config currentConfig;
 
-    public Login(JFrame dialogOwner) {
-        Config currentConfig = new Config();
+  public Login(JFrame dialogOwner) {
+    Gson gsonParser = new Gson();
 
-        login = new JDialog(dialogOwner,"LOGIN",true);
-        login.setSize(290,165);
-        login.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-        login.setLayout(null);
-        login.getContentPane().setBackground(Color.white);
+    try {
+      if (devMode) {
+        configFile = Paths.get(System.getProperty("user.home") + "/BigOneConfig/BigOneConfigDev.json");
+      } else {
+        configFile = Paths.get(System.getProperty("user.home") + "/BigOneConfig/BigOneConfig.json");
+      }
 
-        // check if DevMode ist aktive and make it visible
-        if (currentConfig.isDevMode()) {
-            login.setTitle("LOGIN !!DEVMOD!!");
-            login.getContentPane().setBackground(Color.RED);
+      BufferedReader configReader = Files.newBufferedReader(configFile);
+
+      currentConfig = gsonParser.fromJson(configReader, Config.class);
+
+    } catch (IOException e) {
+      System.err.println("Datei konnte nicht gelesen werden: " + e.getMessage());
+    } catch (Exception e) {
+      System.err.println("Fehler beim JSON-Parsing: " + e.getMessage());
+    }
+
+    login = new JDialog(dialogOwner, "LOGIN", true);
+    login.setSize(290, 165);
+    login.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+    login.setLayout(null);
+    login.getContentPane().setBackground(Color.white);
+
+    // check if DevMode ist aktive and make it visible
+    if (devMode) {
+      login.setTitle("LOGIN !!DEVMOD!!");
+      login.getContentPane().setBackground(Color.RED);
+    }
+
+    lblUeberschrift = new JLabel("Login to BigOne");
+    lblUeberschrift.setBounds(10, 10, 270, 25);
+
+    lblName = new JLabel("Username");
+    lblName.setBounds(10, 40, 100, 25);
+
+    lblPW = new JLabel("Password");
+    lblPW.setBounds(10, 70, 100, 25);
+
+    txtBenutzer = new JTextField();
+    txtBenutzer.setBounds(120, 40, 120, 25);
+    txtBenutzer.setText(currentConfig.getDbUserName());
+
+    txtPW = new JPasswordField("");
+    txtPW.setBounds(120, 70, 120, 25);
+    txtPW.setText(currentConfig.getDbPw());
+
+    btnLogin = new JButton("Login");
+    btnLogin.setBounds(100, 100, 90, 25);
+    btnLogin.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+
+        // zuweisung der Textfeldwerte an die lokalen Varablen
+        strB = txtBenutzer.getText();
+        strPW = new String(txtPW.getPassword());
+
+        try {
+          // Select fitting database driver and connect:
+          Class.forName(currentConfig.getDbDrv());
+          cn = DriverManager.getConnection(currentConfig.getDbUrl() + currentConfig.getDbName(), strB, strPW);
+          login.dispose();
+        } catch (Exception ex) {
+          // ausnahme beschreibung auf der konsole ausgeben
+          txtPW.setText("");
+          txtPW.requestFocus();
+          Logincount++;
+          if (Logincount == 3) {
+            JOptionPane.showMessageDialog(null, "maximale Anzahl der Loginversuche überschritten", "Achtung",
+                JOptionPane.INFORMATION_MESSAGE);
+            login.dispose();
+          }
+          // System.out.println(ex.toString());
         }
 
-        lblUeberschrift = new JLabel("Login to BigOne");
-           lblUeberschrift.setBounds(10,10,270,25);
+      }
+    });
 
-           lblName = new JLabel("Username");
-           lblName.setBounds(10,40,100,25);
+    login.add(lblUeberschrift);
+    login.add(lblName);
+    login.add(lblPW);
+    login.add(txtBenutzer);
+    login.add(txtPW);
+    login.add(btnLogin);
+    login.validate();
+    login.repaint();
+    login.setVisible(true);
 
-           lblPW = new JLabel("Password");
-           lblPW.setBounds(10,70,100,25);
+    txtBenutzer.requestFocus();
+  }
 
-           txtBenutzer = new JTextField();
-           txtBenutzer.setBounds(120,40,120,25);
-           txtBenutzer.setText(currentConfig.getDbUserName());
+  public Connection getConnection() {
+    return cn;
+  }
 
-           txtPW = new JPasswordField("");
-           txtPW.setBounds(120,70,120,25);
-           txtPW.setText(currentConfig.getDbPw());
+  public String getUser() {
+    return txtBenutzer.getText();
+  }
 
-           btnLogin = new JButton("Login");
-           btnLogin.setBounds(100,100,90,25);
-           btnLogin.addActionListener(new ActionListener() {
-               public void actionPerformed(ActionEvent e) {
-
-                   //zuweisung der Textfeldwerte an die lokalen Varablen
-                   strB = txtBenutzer.getText();
-                   strPW = new String(txtPW.getPassword());
-
-                   try {
-                       // Select fitting database driver and connect:
-                       Class.forName( currentConfig.getDbDrv() );
-                       cn = DriverManager.getConnection( currentConfig.getDbUrl() + currentConfig.getDbName(), strB, strPW );
-                       login.dispose();
-                   }
-                   catch( Exception ex ) {
-                       //ausnahme beschreibung auf der konsole ausgeben
-                       txtPW.setText("");
-                       txtPW.requestFocus();
-                       Logincount++;
-                       if(Logincount == 3) {
-                           JOptionPane.showMessageDialog(null, "maximale Anzahl der Loginversuche überschritten", "Achtung", JOptionPane.INFORMATION_MESSAGE);
-                           login.dispose();
-                       }
-                       //System.out.println(ex.toString());
-                   }
-
-               }
-           });
-
-           login.add(lblUeberschrift);
-           login.add(lblName);
-           login.add(lblPW);
-           login.add(txtBenutzer);
-           login.add(txtPW);
-           login.add(btnLogin);
-           login.validate();
-           login.repaint();
-           login.setVisible(true);
-
-           txtBenutzer.requestFocus();
-    }
-    public Connection getConnection() {
-        return cn;
-    }
-    public String getUser() {
-        return txtBenutzer.getText();
-    }
-    public int getLogincount() {
-        return Logincount;
-    }
+  public int getLogincount() {
+    return Logincount;
+  }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,8 @@
 module de.rachel.bigone {
     requires java.sql;
     requires java.desktop;
+    requires jcalendar;
+    requires com.google.gson;
+    // Erlaubt Gson den Reflection-Zugriff auf alle Klassen in diesem Paket
+    opens de.rachel.app to com.google.gson;
 }
